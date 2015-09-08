ead89949ab525e1809336e1754dd9ff9bd02a8d9
Andrzej Mroczek <bartlomiej19@gmail.com>
2015-09-08 14:35:39 +0200

    nbproject dir

--- Postac/Ekwipunek.php
+++ Postac/Ekwipunek.php
@@ -17,30 +17,52 @@ class Ekwipunek {
     private $aktywne;
     private $bron;
     private $zbroja;
+    private $db;
+    private $result;
 
-    public function __construct($ekwipunek, $aktywne) {
-        $this->ekwipunek($ekwipunek);
+    public function __construct($aktywne) {
 
         $this->aktywne($aktywne);
     }
 
-    public function aktywne($aktywne) {
-        if ($aktywne != null) {
-            if ($aktywne[0] != null) {
-                $this->bron = new Bron($aktywne[0]);
+    public function dodajdo_ekwipunku($przedmiot,$name){
+         $this->db=bazadanych::getInstance();
+         $nazwa=$przedmiot[0][nazwa];
+         $typ=$przedmiot[0][typ];
+         $param1=$przedmiot[0][param1];
+         $param2=$przedmiot[0][param2];
+         $cena=$przedmiot[0][cena];
+         $sql="insert into ekwipunek ('user_name', 'nazwa', 'typ', 'param1','param2','cena') values (:name, :nazwa,:typ,:param1,:param2,:cena)";
+         $query=$this->db -> getConnection() -> prepare($sql);
+         $this->result-> execute(array(":name" => $user_name, ":nazwa" => $nazwa, ":typ" => $typ, ":param1" => $param1, ":param2"=>$param2, ":cena"=>$cena));
+    }
+    
+    public function aktywne() {
+        if (isset($this->getpost('wyposaz'))) {
+            if ($this->ekwipunek[0][typ] == 'bron') {
+                $this->bron = new Bron($this->ekwipunek[0][nazwa],$this->ekwipunek[0][param1],$this->ekwipunek[0][param2]);
             }
-            if ($aktywne[1] != null) {
-                $this->zbroja = new Zbroja($aktywne[1]);
+            if ($this->ekwipunek[0][typ] == 'zbroja') {
+                $this->zbroja = new Zbroja($this->ekwipunek[0][nazwa],$this->ekwipunek[0][param1],$this->ekwipunek[0][param2]);
             }
         }
     }
 
-    public function ekwipunek($ekwipunek) {
+    public function showekwipunek($ekwipunek) {
         if ($ekwipunek != null) {
             for ($i = 0; $i < count($ekwipunek); $i++) {
-                $this->ekwipunek = $ekwipunek[$i]['name'] + ' ' + $ekwipunek[$i]['param'] + '/n';
+                $ekwipunek +='<form action="index.php" method="POST">'+$this->ekwipunek[$i][name] + ' '
+                + $ekwipunek[$i][param1] +' '+$ekwipunek[$i][param2]+'<input type="button" value="wyposaz"/>'+ '/n';
             }
         }
     }
+    public function getekwpunek(){
+         $this->db=bazadanych::getInstance();
+         $sql= "select * from ekwipunek";
+         $query = $this->db -> getConnection() -> prepare($sql);
+         $query -> execute();
+         $this->ekwipunek = $query -> fetchAll();
+    }
+   
 
 }
--- Sklep.php
+++ Sklep.php
@@ -19,16 +19,81 @@ class Sklep {
     private $wiedzmin;
     
     private $wiedzmininfo;
+    
+    private $db;
+    
+    private $wynik;
    
-   public function kupuj($przedmioty,Postac\Wiedzmin $wiedzmin){
-               for($i=0; $i<count($przedmioty); $i++){
-       $this->przedmioty+= $przedmioty[$i][nazwa]+$przedmioty[$i][param]+
-                           $przedmioty[$i][cena]+'<input type="submit" value="kupuj">'; 
+    private $zaznaczone;
+   public function obsluga($przedmioty,$transakcja,Postac\Wiedzmin $wiedzmin){ 
+       $this->wynik=$przedmioty;
+       for($i=0; $i<count($przedmioty); $i++){
+       $this->przedmioty+= '<input type="checkbox" name="zaznaczone[]" value='.$przedmioty[$i][nazwa].'>'+$przedmioty[$i][nazwa]+$przedmioty[$i][param]+
+                           $przedmioty[$i][cena]+'<input type="button" value='.$transakcja.'>'; 
      }
      $this->wiedzmin=$wiedzmin;
      
      $this->wiedzmininfo=$this->wiedzmin->getGold+$this->wiedzmin->getName();
      $this->wyswietl=$this->wiedzmininfo+'<form action="index.php" method="POST">'+$this->przedmioty;
      
+}
+public function transakcja(){
+    if($this->getpost('submit')=="kupno"){
+        $zloto=$this->potrzebnezloto();
+        if($this->wiedzmin->getgold()>$zloto){
+         $this->db=$this->db=bazadanych::getInstance();
+         $this->nieposiadanerzeczy();
+         $this->kupno();
+         $this->wiedzmin->setGold($this->wiedzmin->getgold-$zloto);
+     }
+   }
+    elseif($this->getpost('submit')=="sprzedaz"){
+        $zloto=$this->potrzebnezloto();
+        $this->db=$this->db=bazadanych::getInstance();
+        $this->sprzedarz();
+        $this->wiedzmin->setGold($this->wiedzmin->getgold+$zloto);
+    }
+}
+public function kupno(){
+    for($i=0; $i<count($this->wynik); $i++){
+         $nazwa=$this->wynik[$i][nazwa];    
+         $param=$this->wynik[$i][param]; 
+         $sql="insert into ekwipunek ('nazwa','param') values(:nazwa,:param)";
+         $query=$this->db-> prepare($sql);
+         $this->result -> execute(array(":nazwa" => $nazwa,":param"=>$param));
+             }
+         
+     }
+     public function sprzedarz(){
+         for($i=0; $i<count($this->wynik); $i++){
+             $nazwa=$this->zaznaczone[$i][nazwa];
+             $param=$this->zaznaczone[$i][param];
+             $sql= 'DELETE FROM ekwipunek WHERE nazwa=:nazwa';
+             $query=$this->db->prepare($sql);
+             $query->execute(array(':nazwa' => $nazwa));
+         }
+     }
+
+public function potrzebnezloto(){
+    foreach($this->getpost('zaznaczone') as $zaznaczone){
+        for($i=0; $i<cont($this->wynik); $i++){ 
+        if($this->wynik[$i][nazwa]==$zaznaczone){
+            $this->zaznaczone[]=$this->wynik[$i];
+             $zloto+=$this->wynik[$i][cena];
+         }
+        }
+     }
+     return $zloto;
+}
+public function nieposiadanerzeczy(){
+    $this->wynik="";
+    $ekwipunek=$this->wiedzmin->getekwipunek();
+       for($i=0; $i<count($this->zaznaczone); $i++){     
+         for($j=0; $j<cont($ekwipunek); $j++){
+         if($ekwipunek[$j][nazwa]==$this->zaznaczone[$i][nazwa]){
+         $this->wynik[]=$this->zaznaczone[$i];
+          }
+         }    
+         }
 }
 }
